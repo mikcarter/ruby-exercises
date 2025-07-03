972ad67c8056dd3f0cc0d94c57c01703c12fc6ff
mikcarter <michaelanicolegrace@gmail.com>
2025-07-03 17:31:46 +1000

    Hail Lady Athena for her guidance

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,30 +1,62 @@
 
 price1 = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 price2 = [17, 16, 15, 3, 11, 2, 1]
+price3 = [1,2,3,4,5,6,7]
+price4 = [1,1,1,1,1,1,1]
+price5 = [7,6,5,4,3,2,1]
 
 def stock_picker(prices)
-  prices_new = prices
-  price_min = prices_new.index(prices_new.min)
-  price_max = prices_new.index(prices_new.max)
+  price_min = prices.index(prices.min)
+  price_max = prices.index(prices.max)
   if price_min == price_max
+  # case where all values are equal
     puts "slow and steady"
-  elsif price_min == -1
-    prices_new.last(prices_new.size - 1)
-    stock_picker(prices_new)
-  elsif price_max == 0
-    prices_new.drop(1)
-    stock_picker(prices_new)
-  elsif price_min > price_max
-    prices_new.collect { |price| price != prices_new.min}
-    puts "["
-    stock_picker(prices_new)
   else
-    puts prices
-    final_min = prices.index {|e| e == prices_new.min}
-    final_max = prices.index {|e| e == prices_new.max}
-    puts "[#{final_min}, #{final_max}]"
+    # case where largest is first or smallest is last
+    mod_prices = remove_outliers(prices)
+    # case where largest is before smallest
+    mod_prices = remove_smallest(mod_prices)
+    if mod_prices == []
+      # case where numbers are in reverse consecutive order
+      puts "no chance to earn big"
+    else
+      # case where normal
+      final_min = prices.index(mod_prices.min)
+      final_max = prices.index(mod_prices.max)
+      puts "[#{final_min}, #{final_max}]"
+    end
   end
 end
 
+def remove_outliers(prices)
+  price_min = prices.index(prices.min)
+  price_max = prices.index(prices.max)
+  if prices == []
+    []
+  elsif price_min == prices.size - 1
+    remove_outliers(prices.take(prices.size - 1))
+  elsif price_max == 0
+    remove_outliers(prices.drop(1))
+  else
+    prices
+  end
+end
+
+  def remove_smallest(prices)
+    price_min = prices.index(prices.min)
+    price_max = prices.index(prices.max)
+    if prices == []
+      []
+    elsif price_min > price_max
+      remove_smallest(prices.reject { |p| p == prices.at(price_min) })
+    else
+      prices
+    end
+  end
+
 
 stock_picker(price1)
+stock_picker(price2)
+stock_picker(price3)
+stock_picker(price4)
+stock_picker(price5)
